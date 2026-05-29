5bf794458d0e312eab2d4a7f29a976d9c7c42ecc
MikanKi <ivyfel21@gmail.com>
2026-05-29 17:19:59 +0900

    Update SignalFileManager.java
    フィールド、コンストラクタとメソッドを更新した

--- FileManager/SignalFileManager.java
+++ FileManager/SignalFileManager.java
@@ -1,3 +1,29 @@
-public class SignalFileManager extends IFileManager {
 
+import java.io.File;
+
+public class SignalFileManager extends IFileManager {
+    //フィールド
+    private final String[] supportedExtensions = {"bin", "dat", "raw", "txt", "csv"};
+
+    //コンストラクタ
+    public SignalFileManager(){}
+
+    //メソッド
+    public double[] importFile(File file){
+        return null;
+    }
+    public void exportFile(File file, double[] reconstructedSignal){}
+    public boolean isSupportedFileType(File file){
+        return true;
+    }
+    private double[] readBinaryFile(File file){
+        return null;
+    }
+    private double[] readTextFile(File file){
+        return null;
+    }
+    private void writeBinaryFile(File file, double[] reconstructedSignal){}
+    private String getExtension(File file){
+        return null;
+    }
 }
