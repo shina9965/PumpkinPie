6e7149ffe6d8137aa0157fa97471d6cdbe327363
shina9965 <letsDAIshotime@gmail.com>
2026-07-16 21:02:50 +0900

    バグとり

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		nu.pattern.OpenCV.loadShared();
+		nu.pattern.OpenCV.loadLocally();
 		PumpkinPieApplication.main(args);
 	}
 
